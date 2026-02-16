d6ceba33fc9849537af3a1c0606c732f8a0d3761
Junio Roque <jcjunioroque@gmail.com>
2026-02-16 14:35:17 -0300

    exercicio

---
+++ Bradesco_Dio/src/exercicios_Java/_2_Estrutura_de_Controle/D_escreva_numero_inicial.java
@@ -0,0 +1,11 @@
+package exercicios_Java._2_Estrutura_de_Controle;
+
+public class D_escreva_numero_inicial {
+/*
+   Escreva um código onde o usuário informa um número inicial,
+   posteriormente irá informar outros N números,
+   a execução do código irá continuar até que o número informado
+   dividido pelo primeiro número tenha resto diferente de 0 na divisão,
+   números menores que o primeiro número devem ser ignorados
+ */
+}
